503df643942e1981e1e62f23dbe7e4bdb80bb55a
Melissa Malpica <malpicadz@gmail.com>
2014-10-14 17:58:46 +0000

    Adding code to Euler 2 problem #10

--- Euler10.js
+++ Euler10.js
@@ -4,3 +4,9 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.*/
 
+var arrPrime = [ ]
+
+function primes(number) {
+  if (number%number===0) return number;
+}
+
