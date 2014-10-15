9173075af02e0b5325da44e1963eaf16e2db7865
Melissa Malpica <Malpicadz@gmail.com>
2014-10-15 17:27:23 -0400

    adding moves to conway

--- Conway_multi_dimentional_board.js
+++ Conway_multi_dimentional_board.js
@@ -1,6 +1,6 @@
 //multi-dimensional array
 
-var board = [ 
+var board = [
   ['R','N','B','Q','K','B','N','R'],
   ['P','P','P','P','P','P','P','P'],
   [' ',' ',' ',' ',' ',' ',' ',' '],
@@ -16,3 +16,62 @@ console.log(board.join('\n') + '\n\n');
 board[4][4] = board[6][4];
 board[6][4] = ' ';
 console.log(board.join('\n'));
+
+var moves = {
+//White Queens Pawn moves up 2 spots
+
+step1: board[4][3] = board [6][3],
+step2: board[6][3] = ' ',
+console.log(board)
+
+//Black Queens pawn moves forward 2 spots
+step3: board[3][3] = board[1][3]
+step4: board[1][3] = ' ',
+
+//White Knight moves up 2 spots and over 1
+step5: board[5][5] = board[7][6],
+step6: board[7][6] = ' ',
+
+//Black Pawn moves up 1 spot
+step7: board[2][4] = board[1][4],
+step8: board[1][4] = ' ',
+
+//White Knight Pawn moves up 1 spot
+step9: board[5][6] = board[6][6],
+step10: board[6][6] = ' ',
+
+//Black Bishop moves diagnol down and left 1 spot
+step11: board[1][4] = board[0][5],
+step12: board[0][5] = ' ',
+
+//White Bishop's Pawn moves up 2 spots
+step13: board[4][2] = board[6][2],
+step14: board[6][2] = ' ',
+
+//Black Knights move over 1 spot and down 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
