f7f302f9ebdf947982f691745fe22f56ad1037d3
Melissa Malpica <Malpicadz@gmail.com>
2014-10-15 23:11:21 -0400

    WIP Euler prob 2

--- Euler6.js
+++ Euler6.js
@@ -8,7 +8,7 @@ function sqofSum(limit) {
 //square the result of adding all numbers
 
 
-var sum = 0
+/*var sum = 0
 
 for (var i = 0; i < limit; i++) {
 
@@ -19,10 +19,43 @@ for (var i = 0; i < limit; i++) {
 return sum * sum
 
 }
+*/
 
 
+function answer( ) {
 
-describe('sqofSum',function( ){
+  return {
+    sqSum: function( ){
+      var sum100 = 0;
+  for(i=1; i<==100; i++){
+      sum100 += i;
+  }
+
+
+sum100 = Math.pow(sum100,2);
+  return sum100
+};
+
+//END OF SUM SQUARES
+
+sumSq: funtion( ){
+  var sq100 = 0;
+  for(i=1; i<=100; i++) {
+
+    sq100 += Math.pow(i,2);
+
+  }
+    return sq100
+    }
+
+  }// END Return
+
+}//END Answer
+
+console.log(answer)().diff())//
+
+
+describe('sqSum',function( ){
 it('should return square of the sum of given number',function(){
 
   //assert.equal(sqofSum(11),3025);
@@ -30,3 +63,13 @@ it('should return square of the sum of given number',function(){
 
   })
 })
+
+dedscribe('sqSum', function(){
+//   it('squares the sum of natural numbers 1-10', function(){
+//     assert.equal(answer().sqSum(), 3025)
+//   })
+//   it('sums the squares of natural numbers 1-10', function(){
+//     assert.equal(answer().sumSq(), 385)
+//   })
+//   it('finds difference of sum100 and sq100', function(){
+//   assert.equal(answer().diff(), 2640)
